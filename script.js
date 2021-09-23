1b1d2e784fb30d81569777f20bd89aa6787bade0
Maxwell Hankner <maxwellhankner@gmail.com>
2021-09-22 20:50:54 -0500

    basic setup

---
+++ script.js
@@ -0,0 +1,68 @@
+let symbol = '';
+let math = '';
+let calculated = false;
+
+let display = document.getElementById('display');
+let symbolElement = document.getElementById('symbol');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+buttons.map((button) => {
+  button.addEventListener('click', (e) => {
+    switch (e.target.id) {
+      case 'plus':
+        if (calculated) {
+          calculated = false;
+        }
+        symbolElement.innerText = '+';
+        math += '+';
+        display.innerText = '';
+        break;
+      case 'minus':
+        if (calculated) {
+          calculated = false;
+        }
+        symbolElement.innerText = '-';
+        math += '-';
+        display.innerText = '';
+        break;
+      case 'multiply':
+        if (calculated) {
+          calculated = false;
+        }
+        symbolElement.innerText = '*';
+        math += '*';
+        display.innerText = '';
+        break;
+      case 'divide':
+        if (calculated) {
+          calculated = false;
+        }
+        symbolElement.innerText = '/';
+        math += '/';
+        display.innerText = '';
+        break;
+      case 'equal':
+        symbolElement.innerText = '';
+        calculated = true;
+        try {
+          let evaluation = eval(math);
+          display.innerText = evaluation;
+          math = evaluation;
+        } catch {
+          display.innerText = 'Error';
+        }
+        break;
+      default:
+        if (calculated) {
+          calculated = false;
+          math = '';
+          display.innerText = math;
+          math += e.target.id;
+          display.innerText += e.target.id;
+        } else {
+          math += e.target.id;
+          display.innerText += e.target.id;
+        }
+    }
+  });
+});
