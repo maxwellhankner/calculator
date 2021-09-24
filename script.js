5e3fee3658f63c2396ef2cfbfb29c91b49543d7a
Maxwell Hankner <maxwellhankner@gmail.com>
2021-09-23 20:01:16 -0500

    basic time setup

--- script.js
+++ script.js
@@ -1,62 +1,133 @@
+// Display
+let display = document.getElementById('display');
+
+let setDisplay = (string) => {
+  display.innerText = string.toLocaleString(undefined, {
+    minimumFractionDigits: 2,
+  });
+};
+
+let resetDisplay = () => {
+  display.innerText = '';
+};
+
+// watch
+let getCurrentTime = () => {
+  let now = new Date();
+  let hour = now.getHours();
+  hour = hour > 12 ? hour - 12 : hour;
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  return (
+    hour +
+    ' ' +
+    minutes.toLocaleString(undefined, { minimumIntegerDigits: 2 }) +
+    ' ' +
+    seconds.toLocaleString(undefined, { minimumIntegerDigits: 2 })
+  );
+};
+
+let watch;
+
+let updateTime = () => {
+  watch = setInterval(() => {
+    setDisplay(getCurrentTime());
+    // updateTime();
+  }, 1000);
+};
+
+let stopTime = () => {
+  clearInterval(watch);
+};
+
+let buttonWait;
+
+let buttonTimeout = () => {
+  stopTime();
+  resetDisplay();
+  clearTimeout(buttonWait);
+  buttonWait = setTimeout(() => {
+    resetCalculator();
+    setSymbol();
+    updateTime();
+  }, 6000);
+};
+
+updateTime();
+
+// minimumIntegerDigits: 2,
+// minimumFractionDigits: 2
+
+// Calculator
+let symbolElement = document.getElementById('symbol');
+
 let symbol = '';
 let math = '';
 let calculated = false;
 
-let display = document.getElementById('display');
-let symbolElement = document.getElementById('symbol');
+let resetCalculator = () => {
+  symbol = '';
+  math = '';
+  calculated = false;
+};
+
+let setSymbol = (char = '') => {
+  symbolElement.innerText = char;
+};
 
 let buttons = Array.from(document.getElementsByClassName('button'));
 buttons.map((button) => {
   button.addEventListener('click', (e) => {
+    buttonTimeout();
     switch (e.target.id) {
       case 'plus':
         if (calculated) {
           calculated = false;
         }
-        symbolElement.innerText = '\u002B';
+        setSymbol('\u002B');
         math += '+';
-        display.innerText = '';
+        resetDisplay();
         break;
       case 'minus':
         if (calculated) {
           calculated = false;
         }
-        symbolElement.innerText = '\u2212';
+        setSymbol('\u2212');
         math += '-';
-        display.innerText = '';
+        resetDisplay();
         break;
       case 'multiply':
         if (calculated) {
           calculated = false;
         }
-        symbolElement.innerText = '\u00D7';
+        setSymbol('\u00D7');
         math += '*';
-        display.innerText = '';
+        resetDisplay();
         break;
       case 'divide':
         if (calculated) {
           calculated = false;
         }
-        symbolElement.innerText = '\u00F7';
+        setSymbol('\u00F7');
         math += '/';
-        display.innerText = '';
+        resetDisplay();
         break;
       case 'equal':
-        symbolElement.innerText = '';
+        setSymbol('');
         calculated = true;
         try {
           let evaluation = eval(math);
-          display.innerText = evaluation;
+          setDisplay(evaluation);
           math = evaluation;
         } catch {
-          display.innerText = 'Error';
+          setDisplay('Error');
         }
         break;
       default:
         if (calculated) {
           calculated = false;
           math = '';
-          display.innerText = math;
+          setDisplay(math);
           math += e.target.id;
           display.innerText += e.target.id;
         } else {
